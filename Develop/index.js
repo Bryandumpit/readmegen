cee2288c95dd5562baf44fbb6d02f5186135f9ac
Bryan Dumpit <dumpitbr@gmail.com>
2022-08-29 19:49:14 -0400

    add 'placeholder' code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>{
+    return inquirer.createPromptModule([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (rqeuired)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
+            validate: gitHubUser => {
+                if (gitHubUser) {
+                    return true;
+                } else {
+                    console.log('Please enter your github username!')
+                }
+            }
+        },
+    ])
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log('write to file')
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+        .then(writeToFile)
+}
 
 // Function call to initialize app
 init();
