64c92f244deb254be72584455bb3b3650e9e35b4
Bryan Dumpit <dumpitbr@gmail.com>
2022-08-31 11:20:20 -0400

    add switch statement to renderLicenseBadge and if statement for rendering license section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  licenseBadge = ''
+  switch(license){
+    case 'MIT License':
+      licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'GNU GPLv3':
+      licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+    case 'ISC license':
+      licenseBadge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+      break;
+    case 'Apache License 2.0':
+      licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  }
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,12 +24,23 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license){
+    return `
+  ##License:
+  Please click the link to see details about your license
+  [licensename](${renderLicenseLink(license)})
+  `
+  } else {
+    return ''
+  }
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-    # ${data.title} $licenseBadge here
+    # ${data.title} ${renderLicenseBadge(data.license)} 
     ## Description:
       ${data.description}
     ## Table of Contents:
@@ -22,8 +49,8 @@ function generateMarkdown(data) {
       ${data.instruction}
     ## Usage:
       ${data.usage}
-    ifLicense => ## License:
-      $insertlicenselink
+    
+    ${renderLicenseSection(data.license)}
 
     ## Contribution:
 
